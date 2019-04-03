bfd1ef85a8b0e899c253a0667c07055a705d1e32
ronca85 <ronik@hi.t-com.hr>
2019-04-03 23:57:05 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <ninjas></ninjas>
+  </div>
+</template>
+
+<script>
+import Ninjas from './Ninjas.vue'
+
+export default {
+  components: {
+    // <ninjas></ninjas>
+    "ninjas": Ninjas
+  },
+  data () {
+    return {
+      title: "ninja app"
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+	color: purple;
+}
+</style>
