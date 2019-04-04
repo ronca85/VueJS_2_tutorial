de81f5988efcc3dfeec4b5e61a72a695101d88fe
ronca85 <ronik@hi.t-com.hr>
2019-04-04 20:03:05 +0200

    Vue Ninjas v-show example

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+
+
+<script>
+export default {
+  components: {
+  },
+  data () {
+    return {
+      title: "Vue Ninjas"
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+
+
+<style scoped>
+header {
+	background: lightgreen;
+	padding: 10px;
+}
+h1 {
+	color: #222;
+	text-align: center;
+}
+</style>
