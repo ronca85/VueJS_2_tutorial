00461334b075167ef5654aaa9bea2cff22c8b2f4
ronca85 <ronik@hi.t-com.hr>
2019-04-13 22:21:25 +0200

    31-select-box-binding

---
+++ 31-select-box-binding/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <add-blog></add-blog>
+  </div>
+</template>
+
+
+
+<script>
+import addBlog from "./components/addBlog.vue";
+
+export default {
+  components: {
+    "add-blog": addBlog
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+
+
+<style scoped>
+</style>
